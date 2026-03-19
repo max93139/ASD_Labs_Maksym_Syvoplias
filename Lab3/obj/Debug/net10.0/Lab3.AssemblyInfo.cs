4d613babc0b445c0b561842ae55bf62b4faba4fc
max93139 <max1234btc@gmail.com>
2026-03-19 21:57:49 +0200

    start 4 Lab

--- Lab3/obj/Debug/net10.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net10.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40da682f4a440a1219b972a86b7b277e3ed1d846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e09bf3acd371285f964d6875bf2a36f04e2fc8d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab4/Program.cs
@@ -0,0 +1,41 @@
+﻿ using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+    
+            string choice ;
+            do
+            {
+            Console.Clear();
+            Console.WriteLine(" Меню ");
+            Console.WriteLine("1 ");
+            Console.WriteLine("2");
+            Console.WriteLine("0. Вихід");
+            choice = Console.ReadLine() ?? "";
+            switch (choice)
+            {
+                case "1":
+                    
+                    break;
+                case "2":
+                    
+                    break;
+                case "0":
+                    
+                    break;
+                default:
+                    Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+                    break;
+            }
+            Console.WriteLine("Натисніть будь-яку клавішу, щоб продовжити...");
+            Console.ReadKey();
+            Console.Clear();
+            }
+            while (choice != "0");
+    
+        }
+        
+}
